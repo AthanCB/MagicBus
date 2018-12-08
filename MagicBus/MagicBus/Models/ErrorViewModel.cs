8f06526924db65a0898326d5d68c71feea4582cb
Athan Bonis <athancb@hotmail.com>
2018-12-08 22:03:33 +0200

    Just Started!

---
+++ MagicBus/MagicBus/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MagicBus.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
